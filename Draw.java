fec9f90882e463414e38cc728be88b108089d320
Ittipol k <Ittipol.Kaewprasert@krungsri.com>
2023-05-05 09:52:13 +0700

    Code improve

--- Draw.java
+++ Draw.java
@@ -31,10 +31,10 @@ public class Draw extends JPanel implements  MouseListener, MouseMotionListener{
 
     }
 
-    Draw(BufferedImage img){
+    Draw(BufferedImage _img){
 
-        this.img = img;
-        img2  = img;
+        img = _img;
+        img2  = _img;
         dn = new Dimension(Paint.vW,Paint.vH);
         setPreferredSize(dn);
         setBackground(Color.white);
@@ -73,65 +73,65 @@ public class Draw extends JPanel implements  MouseListener, MouseMotionListener{
 
     public void draw(Graphics2D g2){
 
-        if(this.choice == 2)
+        if(choice == 2)
         {
-          if(this.optionR == 0){
+          if(optionR == 0){
                 g2.drawRoundRect(sx, sy, ex-sx, ey-sy, 0, 0);
                 g2.drawRoundRect(ex, ey, sx-ex, sy-ey, 0, 0);
                 g2.drawRoundRect(sx, ey, ex-sx, sy-ey, 0, 0);
                 g2.drawRoundRect(ex, sy, sx-ex, ey-sy, 0, 0);
           }
-          else if(this.optionR == 1){
+          else if(optionR == 1){
                 g2.fillRoundRect(sx, sy, ex-sx, ey-sy, 0, 0);
                 g2.fillRoundRect(ex, ey, sx-ex, sy-ey, 0, 0);
                 g2.fillRoundRect(sx, ey, ex-sx, sy-ey, 0, 0);
                 g2.fillRoundRect(ex, sy, sx-ex, ey-sy, 0, 0);
           }
         }
-        else if(this.choice == 4)
+        else if(choice == 4)
         {
-                if(this.optionO == 0){
+                if(optionO == 0){
                 g2.drawOval(sx, sy, ex-sx, ey-sy);
                 g2.drawOval(ex, ey, sx-ex, sy-ey);
                 g2.drawOval(sx, ey, ex-sx, sy-ey);
                 g2.drawOval(ex, sy, sx-ex, ey-sy);
             }
-            else if(this.optionO == 1){
+            else if(optionO == 1){
                 g2.fillOval(sx, sy, ex-sx, ey-sy);
                 g2.fillOval(ex, ey, sx-ex, sy-ey);
                 g2.fillOval(sx, ey, ex-sx, sy-ey);
                 g2.fillOval(ex, sy, sx-ex, ey-sy);
             }
         }
-        else if(this.choice == 5)
+        else if(choice == 5)
         {
-            if(this.optionRR == 0){
+            if(optionRR == 0){
                 g2.drawRoundRect(sx, sy, ex-sx, ey-sy, 20, 20);
                 g2.drawRoundRect(ex, ey, sx-ex, sy-ey, 20, 20);
                 g2.drawRoundRect(sx, ey, ex-sx, sy-ey, 20, 20);
                 g2.drawRoundRect(ex, sy, sx-ex, ey-sy, 20, 20);
             }
-            else if(this.optionRR == 1){
+            else if(optionRR == 1){
                 g2.fillRoundRect(sx, sy, ex-sx, ey-sy, 20, 20);
                 g2.fillRoundRect(ex, ey, sx-ex, sy-ey, 20, 20);
                 g2.fillRoundRect(sx, ey, ex-sx, sy-ey, 20, 20);
                 g2.fillRoundRect(ex, sy, sx-ex, ey-sy, 20, 20);
             }
         }
-        else if(this.choice == 6){
-            if(this.optionL == 0){
+        else if(choice == 6){
+            if(optionL == 0){
                 g2.setStroke(new BasicStroke(1));
             }
-            else if(this.optionL == 1){
+            else if(optionL == 1){
                 g2.setStroke(new BasicStroke(2));
             }
-            else if(this.optionL == 2){
+            else if(optionL == 2){
                 g2.setStroke(new BasicStroke(3));
             }
-            else if(this.optionL == 3){
+            else if(optionL == 3){
                 g2.setStroke(new BasicStroke(4));
             }
-            else if(this.optionL == 4){
+            else if(optionL == 4){
                 g2.setStroke(new BasicStroke(5));
             }
                 g2.drawLine(sx, sy, ex, ey);
@@ -158,51 +158,51 @@ public class Draw extends JPanel implements  MouseListener, MouseMotionListener{
         }
 
 
-        if(this.choice == 0){
+        if(choice == 0){
             Paint.checkD = 0;
             gi.drawLine(sx, sy, sx, sy);
         }
-        else if(this.choice == 1){
+        else if(choice == 1){
             Paint.checkD = 0;
-            if(this.optionB == 0){
+            if(optionB == 0){
                 gi.setStroke(new BasicStroke(6,1,1));
             }
-            else if(this.optionB == 1)
+            else if(optionB == 1)
             {
                 gi.setStroke(new BasicStroke(4,1,1));
             }
-            else if(this.optionB == 2)
+            else if(optionB == 2)
             {
                 gi.setStroke(new BasicStroke(1,1,1));
             }
-            else if(this.optionB == 3)
+            else if(optionB == 3)
             {
                 gi.setStroke(new BasicStroke(6,2,0));
             }
-            else if(this.optionB == 4)
+            else if(optionB == 4)
             {
                 gi.setStroke(new BasicStroke(4,2,0));
             }
-            else if(this.optionB == 5)
+            else if(optionB == 5)
             {
                 gi.setStroke(new BasicStroke(1,2,0));
             }
             gi.drawLine(sx, sy, sx, sy);
         }
-        else if(this.choice == 3){
+        else if(choice == 3){
         }
-        else if(this.choice == 7){
+        else if(choice == 7){
             Paint.checkD = 0;
-            if(this.optionE == 0){
+            if(optionE == 0){
                 gi.setStroke(new BasicStroke(2));
             }
-            else if(this.optionE == 1){
+            else if(optionE == 1){
                 gi.setStroke(new BasicStroke(4));
             }
-            else if(this.optionE == 2){
+            else if(optionE == 2){
                 gi.setStroke(new BasicStroke(6));
             }
-            else if(this.optionE == 3){
+            else if(optionE == 3){
                 gi.setStroke(new BasicStroke(8));
             }
             gi.setColor(Color.white);
@@ -226,70 +226,70 @@ public class Draw extends JPanel implements  MouseListener, MouseMotionListener{
             color = Paint.plC2.getBackground();
         }
 
-        if(this.choice == 2)
+        if(choice == 2)
         {
             Paint.checkD = 0;
-            if(this.optionR == 0){
+            if(optionR == 0){
                 gi.drawRoundRect(sx, sy, ex-sx, ey-sy, 0, 0);
                 gi.drawRoundRect(ex, ey, sx-ex, sy-ey, 0, 0);
                 gi.drawRoundRect(sx, ey, ex-sx, sy-ey, 0, 0);
                 gi.drawRoundRect(ex, sy, sx-ex, ey-sy, 0, 0);
           }
-          else if(this.optionR == 1){
+          else if(optionR == 1){
                 gi.fillRoundRect(sx, sy, ex-sx, ey-sy, 0, 0);
                 gi.fillRoundRect(ex, ey, sx-ex, sy-ey, 0, 0);
                 gi.fillRoundRect(sx, ey, ex-sx, sy-ey, 0, 0);
                 gi.fillRoundRect(ex, sy, sx-ex, ey-sy, 0, 0);
           }
         }
-        else if(this.choice == 4)
+        else if(choice == 4)
         {
             Paint.checkD = 0;
-            if(this.optionO == 0){
+            if(optionO == 0){
                 gi.drawOval(sx, sy, ex-sx, ey-sy);
                 gi.drawOval(ex, ey, sx-ex, sy-ey);
                 gi.drawOval(sx, ey, ex-sx, sy-ey);
                 gi.drawOval(ex, sy, sx-ex, ey-sy);
             }
-            else if(this.optionO == 1){
+            else if(optionO == 1){
                 gi.fillOval(sx, sy, ex-sx, ey-sy);
                 gi.fillOval(ex, ey, sx-ex, sy-ey);
                 gi.fillOval(sx, ey, ex-sx, sy-ey);
                 gi.fillOval(ex, sy, sx-ex, ey-sy);
             }
         }
-        else if(this.choice == 5)
+        else if(choice == 5)
         {
             Paint.checkD = 0;
-           if(this.optionRR == 0){
+           if(optionRR == 0){
                 gi.drawRoundRect(sx, sy, ex-sx, ey-sy, 20, 20);
                 gi.drawRoundRect(ex, ey, sx-ex, sy-ey, 20, 20);
                 gi.drawRoundRect(sx, ey, ex-sx, sy-ey, 20, 20);
                 gi.drawRoundRect(ex, sy, sx-ex, ey-sy, 20, 20);
             }
-            else if(this.optionRR == 1){
+            else if(optionRR == 1){
                 gi.fillRoundRect(sx, sy, ex-sx, ey-sy, 20, 20);
                 gi.fillRoundRect(ex, ey, sx-ex, sy-ey, 20, 20);
                 gi.fillRoundRect(sx, ey, ex-sx, sy-ey, 20, 20);
                 gi.fillRoundRect(ex, sy, sx-ex, ey-sy, 20, 20);
             }
         }
-        else if(this.choice == 6)
+        else if(choice == 6)
         {
             Paint.checkD = 0;
-            if(this.optionL == 0){
+            if(optionL == 0){
                 gi.setStroke(new BasicStroke(1));
             }
-            else if(this.optionL == 1){
+            else if(optionL == 1){
                 gi.setStroke(new BasicStroke(2));
             }
-            else if(this.optionL == 2){
+            else if(optionL == 2){
                 gi.setStroke(new BasicStroke(3));
             }
-            else if(this.optionL == 3){
+            else if(optionL == 3){
                 gi.setStroke(new BasicStroke(4));
             }
-            else if(this.optionL == 4){
+            else if(optionL == 4){
                 gi.setStroke(new BasicStroke(5));
             }
                 gi.drawLine(sx, sy, ex, ey);
@@ -318,33 +318,33 @@ public class Draw extends JPanel implements  MouseListener, MouseMotionListener{
         }
 
 
-        if(this.choice == 0){
+        if(choice == 0){
             gi.drawLine(sx, sy, ex, ey);
             sx = ex;
             sy = ey;
         }
 
-        else if(this.choice == 1){
-            if(this.optionB == 0){
+        else if(choice == 1){
+            if(optionB == 0){
                 gi.setStroke(new BasicStroke(6,1,1));
             }
-            else if(this.optionB == 1)
+            else if(optionB == 1)
             {
                 gi.setStroke(new BasicStroke(4,1,1));
             }
-            else if(this.optionB == 2)
+            else if(optionB == 2)
             {
                 gi.setStroke(new BasicStroke(1,1,1));
             }
-            else if(this.optionB == 3)
+            else if(optionB == 3)
             {
                 gi.setStroke(new BasicStroke(6,2,0));
             }
-            else if(this.optionB == 4)
+            else if(optionB == 4)
             {
                 gi.setStroke(new BasicStroke(4,2,0));
             }
-            else if(this.optionB == 5)
+            else if(optionB == 5)
             {
                 gi.setStroke(new BasicStroke(1,2,0));
             }
@@ -352,19 +352,19 @@ public class Draw extends JPanel implements  MouseListener, MouseMotionListener{
             sx = ex;
             sy = ey;
         }
-        else if(this.choice == 3){  
+        else if(choice == 3){  
         }
-        else if(this.choice == 7){
-            if(this.optionE == 0){
+        else if(choice == 7){
+            if(optionE == 0){
                 gi.setStroke(new BasicStroke(2));
             }
-            else if(this.optionE == 1){
+            else if(optionE == 1){
                 gi.setStroke(new BasicStroke(4));
             }
-            else if(this.optionE == 2){
+            else if(optionE == 2){
                 gi.setStroke(new BasicStroke(6));
             }
-            else if(this.optionE == 3){
+            else if(optionE == 3){
                 gi.setStroke(new BasicStroke(8));
             }
             gi.setColor(Color.white);
@@ -377,7 +377,7 @@ public class Draw extends JPanel implements  MouseListener, MouseMotionListener{
     }
 
     public void mouseMoved(MouseEvent e) {
-        if(this.choice != 10)
+        if(choice != 10)
             setCursor(new Cursor(1));
     }
 
